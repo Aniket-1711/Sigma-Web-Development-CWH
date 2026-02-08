95fc8e61f6aef47a55e49c9dbbbd2d004927c4a9
Aniket-1711 <gawandeaniketh@gmail.com>
2026-02-08 12:34:50 +0530

    feat: add examples to demonstrate the router() in express and it's uses

--- 89.Request Response and routers in Express/script.js
+++ 89.Request Response and routers in Express/script.js
@@ -1,8 +1,19 @@
 const express=require("express")
+const blogs=require("./routes/blogs")
+const shops=require("./routes/shops")
 
 const app=express()
 
-app.use(express.static("public"))
+//* finally let's see the routing - routing helps in organizing like the following example
+//!here if we have to handle a blogs page we will mess and increase the lines of code in the current file, so with the help of routing we separately handle the blogs requests using router
+
+app.use('/blog', blogs)
+//!what the above line is saying ki - the requests that start with /blog will be handled by blogs (which is ./routes/blogs/blog)
+
+
+//todo similarly we can do or shopping site also - import it and let app.use handle it
+app.use('/shop', shops)
+
 
 app.get("/", (req,res)=>{
     console.log("Hey it's a get request")
@@ -39,10 +50,12 @@ app.get("/home", (req, res)=>{
     res.sendFile("templates/homepage.html", {root:__dirname})
 })
 
+app.use(express.static("public"))
 
 app.listen(3000, ()=>{
     console.log("Server listening at port 3000")
 })
+
 
 
 //! there is a limit of 2048 characters, we can't send more than these many characters in the get request (get request is basically that we hit using browser)
