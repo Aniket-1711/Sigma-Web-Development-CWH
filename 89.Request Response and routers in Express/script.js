3cdb2f11a01fa46d04211d79ab66ad155afaa264
Aniket-1711 <gawandeaniketh@gmail.com>
2026-02-08 10:43:04 +0530

    feat: handle put request and  request chaining

--- 89.Request Response and routers in Express/script.js
+++ 89.Request Response and routers in Express/script.js
@@ -14,9 +14,29 @@ app.post("/", (req,res)=>{
     res.send("Hello From Post")
 })
 
-app.listen(3000, ()=>{
-    console.log("Server listening at port 3000")
+
+app.put("/", (req,res)=>{
+    console.log("Hey it's a put request")
+    res.send("Hello From Put")
 })
+
+
+//*the similar above thing can also be chanied as follows and will work the same
+//todo app.get("/", (req,res)=>{
+//todo     console.log("Hey it's a get request")
+//todo     res.send("Hello World")
+//todo }).post("/", (req,res)=>{
+//todo     console.log("Hey it's a post request")
+//todo     res.send("Hello From Post")
+//todo }).put("/", (req,res)=>{
+//todo     console.log("Hey it's a put request")
+//todo     res.send("Hello From Put")
+//todo }).listen(3000, ()=>{
+//todo     console.log("Server listening at port 3000")
+//todo })
+
+
+
 
 //! there is a limit of 2048 characters, we can't send more than these many characters in the get request (get request is basically that we hit using browser)
 
